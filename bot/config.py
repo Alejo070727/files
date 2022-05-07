5ee6b25c251089d689987147fa91864db4af9e09
Alejo070727 <105022027+Alejo070727@users.noreply.github.com>
2022-05-07 18:15:02 -0400

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -3,43 +3,43 @@ from os import getenv
 # TELEGRAM
 OWNER = int(getenv('OWNER'))
 if OWNER is None:
-    raise Exception("Por favor configura tu Telegram User ID") 
+    raise Exception("1232940388") 
 
 API_ID = int(getenv('API_ID'))
 if API_ID is None:
-    raise Exception("Por favor configura el API_ID del Bot") 
+    raise Exception("14429012") 
 
 API_HASH = getenv('API_HASH')
 if API_HASH is None:
-    raise Exception("Por favor configura el API_HASH del Bot") 
+    raise Exception("074d67326cfbcdc5550aeb7aac69d424") 
 
 TELEGRAM_TOKEN = getenv('TELEGRAM_TOKEN')
 if TELEGRAM_TOKEN is None:
-    raise Exception("Por favor configura el TOKEN del Bot") 
+    raise Exception("5113761710:AAHqGq2BY33aucp3OL850siQ4UQPif5F5C0") 
 
 # DATOS DEL MOODLE
 HOST = getenv('HOST')
 if HOST is None:
-    raise Exception("Por favor configura la URL del Moodle") 
+    raise Exception("https://eduvirtual.uho.edu.cu/") 
 
 ACCOUNT = getenv('ACCOUNT')
 if ACCOUNT is None:
-    raise Exception("Por favor configura tu nombre de usuario del Moodle") 
+    raise Exception("yeima") 
 
 PASSWORD = getenv('PASSWORD')
 if PASSWORD is None:
-    raise Exception("Por favor configura tu contraseña del Moodle") 
+    raise Exception("*Yordan1") 
 
 # CUENTA DE MEGA
 PASS_MEGA = getenv('PASS_MEGA')
 if PASS_MEGA is None:
-    raise Exception("Por favor configura tu contraseña de GMAIL") 
+    raise Exception("danipupo27") 
 
 GMAIL_MEGA = getenv('GMAIL_MEGA')
 if GMAIL_MEGA is None:
-    raise Exception("Por favor configura tu correo de GMAIL") 
+    raise Exception("pupodani7@gmail.com") 
 
 # ARCHIVOS
 MEGABYTES = int(getenv('MEGABYTES'))
 if MEGABYTES is None:
-    raise Exception("Por favor configura los MEGABYTES a los que se dividirán los archivos") 
+    raise Exception("150") 
